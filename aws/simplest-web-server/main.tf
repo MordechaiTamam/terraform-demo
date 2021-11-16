c196329c2d922a4fc2e14991d330cc7c4b8a4649
modi <modi.tamam@gmail.com>
2021-11-16 19:51:04 +0200

    Added security group to allow all TLS connections from my IP. Added a task "wait for connections" to playbook.

--- aws/simplest-web-server/main.tf
+++ aws/simplest-web-server/main.tf
@@ -1,7 +1,36 @@
 provider "aws" {
   region = "us-east-1"
 }
+data "http" "myip"{
+    url = "https://ipv4.icanhazip.com"
+}
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+
+  ingress {
+        # TCP (change to whatever ports you need)
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        # Please restrict your ingress to only necessary IPs and ports.
+        # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+        cidr_blocks = ["${chomp(data.http.myip.body)}/32"]
+      }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -16,6 +45,7 @@ resource "aws_instance" "myInstance" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
   key_name = "ubuntu-devops-experts"
+  security_groups = ["${aws_security_group.allow_tls.name}"]
   tags = {
     key ="my key"
   }
